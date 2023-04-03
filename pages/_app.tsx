23de7d9f78791b2ae3b4e0216fff9e1ea52f231e
Rishabh Agrawal <r.agrawal@dyninno.in>
2023-04-03 16:48:43 +0530

    add loadbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
+
+import ProgressBar from '@badrap/bar-of-progress';
+
+const progress = new ProgressBar({
+  size: 5,
+  color : "#FE595E",
+  className : 'z-50',
+  delay: 100
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
